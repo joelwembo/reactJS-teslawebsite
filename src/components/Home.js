c71811099396eef896cbc9e8f82743bbb3547e05
Joel Wembo <joelotepawembo@gmail.com>
2021-05-19 19:29:34 +0800

    added new components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import styles from "styled-components";
+import styled from "styled-components";
 import Section from './Section';
 
 
 function Home() {
     return (
         <Container>
-            <Section />
+            <Section 
+                title = "Model s"
+                description ="Order Online for Touchless Delivery"
+                backgroundImg ="model-s.jpg"
+                leftBtnText ="Custom Order"
+                rightBtnText = "Existing Inventory"
+            
+            />
+            <Section
+                title = "Model y"
+                description ="Order Online for Touchless Delivery"
+                backgroundImg ="model-y.jpg" 
+                leftBtnText ="Custom Order"
+                rightBtnText = "Existing Inventory"
+            />
+            <Section
+                title = "Model x"
+                description ="Order Online for Touchless Delivery"
+                backgroundImg ="model-x.jpg" 
+                leftBtnText ="Custom Order"
+                rightBtnText = "Existing Inventory"
+            
+            
+            />
+
+            <Section
+                title = "Solar for new Panel"
+                description ="Order Online for Touchless Delivery"
+                backgroundImg ="solar-roof.jpg" 
+                leftBtnText ="Order Now"
+                rightBtnText = "Learn"
+            
+            
+            />
+
+                <Section
+                title = "Accessories"
+                description ="Order Online for Touchless Delivery"
+                backgroundImg ="accessories.jpg" 
+                leftBtnText ="Order Now"
+                        
+                />
         </Container>
     )
 }
 
 export default Home
-
-const Container = styles.div`
-    height: 100vh;
+ 
+const Container = styled.div`
+    height: 100vh;`
